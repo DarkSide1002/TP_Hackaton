8684e2a3deb3847c83a1864f16765eb13287bebb
AndreyA <andrew.arustamov.com@gmail.com>
2026-05-31 19:44:12 +0000

    Создал файл-модуль с программой, которая перемещает файл-письмо в папку соответствующей категории с помощью метода move, возвращающего новый путь файла

---
+++ src/file_manager.py
@@ -0,0 +1,19 @@
+import os
+import shutil
+from pathlib import Path
+
+
+class FileManager:
+    def __init__(self, root: str):
+        self.root = root
+
+    def create(self, lst: list):
+        for c in lst:
+            Path(os.path.join(self.root, c)).mkdir(parents=True, exist_ok=True)
+
+    def move(self, old_path: str, c: str) -> str:
+        dest = os.path.join(self.root, c)
+        name = os.path.basename(old_path)
+        new_path = os.path.join(dest, name)
+        shutil.move(old_path, new_path)
+        return new_path
